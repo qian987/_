19facbabc3da016825094af1fab83aa75585c953
qian987 <71359545+qian987@users.noreply.github.com>
2020-10-20 17:03:10 +0800

    测试

--- src/test/java/TestScoreManage.java
+++ src/test/java/TestScoreManage.java
@@ -6,7 +6,7 @@ public class TestScoreManage {
     @Test
     public void Test_01(){
         //虚假学生数据
-        //1
+        //2
 
         String [][] str=new String[][]{
                 {"学号","姓名","语文成绩","数学成绩","英语成绩"},
